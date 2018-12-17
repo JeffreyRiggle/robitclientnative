099d7c68d5bbb3bca3cc369e0ae2252cb7e0972f
JeffreyRiggle <JeffreyRiggle@gmail.com>
2018-12-17 06:42:40 -0500

    initial server work

--- src/main/ipcBridge.js
+++ src/main/ipcBridge.js
@@ -4,6 +4,14 @@ const start = () => {
     ipcMain.on('healthcheck', (event) => {
         event.sender.send('heartbeat');
     });
+
+    ipcMain.on('startserver', (event) => {
+        event.sender.send('serverstate', 'loading');
+        
+        setTimeout(() => {
+            event.sender.send('serverstate', 'started');
+        }, 5000)
+    })
 }
 
 export default start
