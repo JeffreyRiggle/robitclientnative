22beb6a9fb4020daf74f724367d849fed1230d67
JeffreyRiggle <JeffreyRiggle@gmail.com>
2018-12-19 04:43:03 -0500

    Add process metrics for windows users

--- src/main/serverManager.js
+++ src/main/serverManager.js
@@ -1,14 +1,86 @@
 import fs from 'fs';
-import { spawn } from 'child_process';
+import { spawn, exec } from 'child_process';
 import { registerEvent, broadcast } from './ipcBridge';
 
 const appData = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + 'Library/Preferences' : '/var/local');
 const dir = appData + '/robitserver';
 const serverContents = fs.readFileSync(__dirname + '/../../builtServer/bundle.js');
 const stateevent = 'serverstate';
+const dayInterval = 1 * 24 * 60 * 60 * 1000;
+const hourInterval = 1 * 60 * 60 * 1000;
+const minuteInterval = 1 * 60 * 1000;
+const secondInterval = 1000;
 
-let childProc;
+let childProc, serverUsagePoll, upTime;
 let state = 'stopped';
+
+function formatUpTime(ms) {
+    let days = 0, hours = 0, minutes = 0, seconds = 0;
+
+    while(ms >= dayInterval) {
+        days++;
+        ms -= dayInterval;
+    }
+
+    while(ms >= hourInterval) {
+        hours++;
+        ms -= hourInterval
+    }
+
+    while(ms >= minuteInterval) {
+        minutes++;
+        ms -= minuteInterval
+    }
+
+    while(ms >= secondInterval) {
+        seconds++;
+        ms -= secondInterval;
+    }
+
+    return `${days}:${hours <= 9 ? '0' : ''}${hours}:${minutes <= 9 ? '0' : ''}${minutes}:${seconds <= 9 ? '0' : ''}${seconds}:${ms}`
+}
+
+function sendServerHealth(memory, cpu) {
+    let liveTime = 'N/A';
+
+    if (upTime) {
+        liveTime = formatUpTime((Date.now() - upTime));
+    }
+
+    broadcast('serverhealth', {
+        memory: memory ? memory : 'N/A',
+        cpu: cpu ? cpu : 'N/A',
+        upTime: liveTime
+    });
+}
+
+function getServerUsage() {
+    if (process.platform !== 'win32') {
+        console.log(`Health check is not supported on ${process.platform}`);
+        sendServerHealth();
+        return;
+    }
+
+    exec(`tasklist /v /fi "PID eq ${childProc.pid}" /fo csv`, (err, stdout, stderr) => {
+        if (err) {
+            console.log(`Got error ${err} when attempting to read task list`);
+            return;
+        }
+
+        let info = stdout.toString().split('\n')[1];
+        let [imageName, procId, sessionName, sessionNum, mem, status, userName, cpu, title] = info.split('","');
+
+        sendServerHealth(mem, cpu);
+    });
+}
+
+function startPollingServerUsage() {
+    upTime = Date.now();
+    getServerUsage();
+    serverUsagePoll = setInterval(() => {
+        getServerUsage();
+    }, 10000);
+}
 
 function startServer(event, config) {
     updateStateAndBroadCast('loading');
@@ -32,6 +104,7 @@ function startServer(event, config) {
     });
 
     updateStateAndBroadCast('started');
+    startPollingServerUsage();
 }
 
 function stopServer() {
@@ -43,7 +116,15 @@ function stopServer() {
 
     childProc.kill();
     childProc = null;
+
+    if (serverUsagePoll) {
+        clearInterval(serverUsagePoll);
+        serverUsagePoll = null;
+        upTime = null;
+    }
+
     updateStateAndBroadCast('stopped');
+    sendServerHealth();
 }
 
 function updateStateAndBroadCast(newstate) {
